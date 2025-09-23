c88e35520881032d275b691dc6bd6150b8ca02b7
Pragbi <mahokofr@gmail.com>
2025-09-23 17:43:19 +0200

    #2 Anpassungen Copy to Clipboard

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
 document.getElementById('lastmod').textContent =
     new Date(document.lastModified).toLocaleDateString('de-DE');
 
-const copyToClipboard = async function(elementid, copystring) {
-    document.getElementById(elementid).addEventListener("click", async () => {
-    try {
-        await navigator.clipboard.writeText(copystring);
-        const btn = document.getElementById(elementid);
-        const old = btn.textContent;
-        btn.textContent = " Kopiert!";
-        setTimeout(() => (btn.textContent = old), 1200);
-    } catch (e) {
-        const ta = document.createElement("textarea");
-        ta.value = copystring;
-        ta.style.position = "fixed";
-        ta.style.left = "-9999px";
-        document.body.appendChild(ta);
-        ta.select();
-        try { document.execCommand("copy"); } catch {}
-        document.body.removeChild(ta);
-    }
-    });    
-};
+const btnphone = document.getElementById("copyPhone");
+const msgphone = document.getElementById("copyMsgPhone");
 
-const textToCopy = "Hallo Welt das ist mein String ✨";
-copyToClipboard("copyMail", "webmaster@max-hofert.de");
-copyToClipboard("copyPhone", "+4917687826888");
-copyToClipboard("copyLinkedIn", textToCopy);
+btnphone.addEventListener("click", () => {
+  navigator.clipboard.writeText("+4917687826888");
+  msgphone.style.display = "inline";
+  setTimeout(() => (msgphone.style.display = "none"), 1500);
+});
 
+const btnmail = document.getElementById("copyMail");
+const msgmail = document.getElementById("copyMsgMail");
 
+btnmail.addEventListener("click", () => {
+  navigator.clipboard.writeText("webmaster@max-hofert.de");
+  msgmail.style.display = "inline";
+  setTimeout(() => (msgmail.style.display = "none"), 1500);
+});
